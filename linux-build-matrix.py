5d20b26143c1a0d83b55df79b20149a274e6c38f
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2024-07-29 20:34:59 +0200

    Use kernel tarballs (#1)
    The previous way of using tags from gregkh's git repo seems to be unreliable

--- linux-build-matrix.py
+++ linux-build-matrix.py
@@ -17,28 +17,24 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        version:
+        kernel:
 '''
 
 post = '''    steps:
     - uses: actions/checkout@v4
-      with:
-        repository: gregkh/linux
-        fetch-depth: 1
-        ref: v${{ matrix.version }}
+    - run: wget --quiet --output-document kernel.tar.xz ${{ matrix.kernel.source }} && tar xf kernel.tar.xz
     - run: sudo apt-get update && sudo apt-get -y install apt-utils bc lz4 binutils bison build-essential ca-certificates cscope debhelper dwarves flex gcc git libelf-dev libncurses-dev libssl-dev linux-source make openssl pahole perl-base pkg-config python3-debian python-is-python3 rsync vim
-    - name: Configure
+    - name: Build kernel
       run: |
+        cd linux-${{ matrix.kernel.version }}
         make defconfig
         make kvm_guest.config
-    - name: Build
-      run: |
         make -j`nproc`
     - name: Upload a Build Artifact
       uses: actions/upload-artifact@v4.3.4
       with:
-        name: bzImage-${{ matrix.version }}
-        path: arch/x86_64/boot/bzImage
+        name: bzImage-${{ matrix.kernel.version }}
+        path: linux-${{ matrix.kernel.version }}/arch/x86_64/boot/bzImage
 '''
 
 kernel_versions = ''
@@ -46,9 +42,11 @@ with urlopen('https://www.kernel.org/releases.json') as response:
     releases = json.loads(response.read().decode())['releases']
     for r in releases:
         version = r['version']
+        source = r['source']
         if 'next' not in version:
-            print(f'          - "{version}"')
-            kernel_versions = kernel_versions + f'          - "{version}"\n'
+            print(version)
+            kernel_versions = kernel_versions + f'          - version: "{version}"\n'
+            kernel_versions = kernel_versions + f'            source: "{source}"\n'
 
 workflow = pre + kernel_versions + post
 
