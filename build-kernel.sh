418d835836ee63b4a21902d101406e58e59c69bb
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-10-28 21:39:06 +0100

    fix

--- build-kernel.sh
+++ build-kernel.sh
@@ -4,9 +4,3 @@ make defconfig
 make kvm_guest.config
 echo 'CONFIG_EFI_STUB=y' >> .config
 make -j$(nproc)
-
-# User Mode Linux
-make mrproper
-make mrproper ARCH=um
-make defconfig ARCH=um
-make -j$(nproc) ARCH=um
